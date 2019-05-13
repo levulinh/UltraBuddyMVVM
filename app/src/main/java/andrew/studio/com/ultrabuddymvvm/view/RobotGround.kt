122bd541e7aeb514cb20322edcb77d68bd223933
Linh Le Vu <levulinhchv@gmail.com>
2019-05-13 15:07:17 +0700

    Update Map Drawing to look like Google Maps app

--- app/src/main/java/andrew/studio/com/ultrabuddymvvm/view/RobotGround.kt
+++ app/src/main/java/andrew/studio/com/ultrabuddymvvm/view/RobotGround.kt
@@ -2,27 +2,48 @@ package andrew.studio.com.ultrabuddymvvm.view
 
 import andrew.studio.com.ultrabuddymvvm.R
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import kotlin.math.roundToInt
 
+private const val INNER_DOT_RADIUS = 20f
+private const val OUTER_DOT_RADIUS = 30f
+private const val TRIANGLE_OFFSET = 35
+private const val TRIANGLE_HEIGHT = 10
+private const val TRIANGLE_WIDTH = 30
+
+private const val CONE_HEIGHT = 100
+private const val CONE_RADIUS = 20 + INNER_DOT_RADIUS
+
 class RobotGround(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
+    private val groundColor = getColorFromId(R.color.lightBlue)
+    private val groundStrokeColor = getColorFromId(R.color.darkBlue)
+    private val obstacleColor = getColorFromId(R.color.darkGray)
+    private val testPointColor = getColorFromId(R.color.red)
 
-    private var groundColor = getColorFromId(R.color.lightBlue)
-    private var groundStrokeColor = getColorFromId(R.color.darkBlue)
-    private var obstacleColor = getColorFromId(R.color.darkGray)
-    private var testPointColor = getColorFromId(R.color.red)
+    private val white = getColorFromId(R.color.white)
 
-    private var boderWidth = 4.0f
+    private val robotDotInnerColor = getColorFromId(R.color.red)
+    private val robotDotOuterColor = getColorFromId(R.color.clearLightRed)
+    private val robotDirectionColor = getColorFromId(R.color.lightRed)
+    private val robotDirectionColorTransparent = getColorFromId(R.color.transparent)
 
-    private var groundWidth = 100
-    private var groundHeight = 80
+    private val borderWidth = 4.0f
+
+    private val groundWidth = 100
+    private val groundHeight = 80
+
+    object Directions {
+        const val EAST = 0
+        const val WEST = 1
+        const val SOUTH = 2
+        const val NORTH = 3
+
+    }
 
     private val unit: Float
         get() {
@@ -45,19 +66,118 @@ class RobotGround(context: Context, attrs: AttributeSet) : View(context, attrs) 
         super.onDraw(canvas)
 
         // Translate the canvas to Descartes Coordination system and center vertical the ground
-        canvas.translate(0f, (height.toFloat() - groundHeight.toUltraUnit()) / 2 + groundHeight)
+        canvas.translate(0f, (height.toFloat() - groundHeight.toUltraUnit()) / 2 + groundHeight.toUltraUnit())
         canvas.scale(1f, -1f)
 
         // Do the drawings
         drawGround(canvas)
-        drawTestPoint(canvas)
+        drawRobot(canvas, x = 30, y = groundHeight / 2, dir = Directions.EAST)
     }
 
-    private fun drawTestPoint(canvas: Canvas) {
-        paint.color = testPointColor
+    private fun drawRobot(canvas: Canvas, x: Int, y: Int, dir: Int) {
+        val xUnit = x.toUltraUnit()
+        val yUnit = y.toUltraUnit()
+
+        paint.color = robotDotOuterColor
         paint.style = Paint.Style.FILL
 
-        canvas.drawCircle(30.toUltraUnit(), (groundHeight / 2).toUltraUnit(), 10f, paint)
+        canvas.drawCircle(xUnit, yUnit, OUTER_DOT_RADIUS, paint)
+
+        paint.color = robotDotInnerColor
+        paint.style = Paint.Style.FILL
+
+        val directionTriangle: Path = when (dir) {
+            Directions.NORTH -> {
+                Path().apply {
+                    moveTo(xUnit - INNER_DOT_RADIUS, yUnit)
+                    lineTo(xUnit + INNER_DOT_RADIUS, yUnit)
+                    lineTo(xUnit + CONE_RADIUS, yUnit + CONE_HEIGHT)
+                    lineTo(xUnit - CONE_RADIUS, yUnit + CONE_RADIUS)
+                    close()
+                }
+            }
+            Directions.SOUTH -> {
+                Path().apply {
+                    moveTo(xUnit - INNER_DOT_RADIUS, yUnit)
+                    lineTo(xUnit + INNER_DOT_RADIUS, yUnit)
+                    lineTo(xUnit + CONE_RADIUS, yUnit - CONE_HEIGHT)
+                    lineTo(xUnit - CONE_RADIUS, yUnit - CONE_RADIUS)
+                    close()
+                }
+            }
+            Directions.EAST -> {
+                Path().apply {
+                    moveTo(xUnit, yUnit + INNER_DOT_RADIUS)
+                    lineTo(xUnit, yUnit - INNER_DOT_RADIUS)
+                    lineTo(xUnit + CONE_HEIGHT, yUnit - CONE_RADIUS)
+                    lineTo(xUnit + CONE_HEIGHT, yUnit + CONE_RADIUS)
+                    close()
+                }
+            }
+            else -> {
+                Path().apply {
+                    moveTo(xUnit, yUnit + INNER_DOT_RADIUS)
+                    lineTo(xUnit, yUnit - INNER_DOT_RADIUS)
+                    lineTo(xUnit - CONE_HEIGHT, yUnit - CONE_RADIUS)
+                    lineTo(xUnit - CONE_HEIGHT, yUnit + CONE_RADIUS)
+                    close()
+                }
+            }
+        }
+
+        paint.shader = when (dir) {
+            Directions.NORTH -> LinearGradient(
+                xUnit,
+                yUnit,
+                xUnit,
+                yUnit + CONE_HEIGHT,
+                robotDirectionColor,
+                robotDirectionColorTransparent,
+                Shader.TileMode.MIRROR
+            )
+            Directions.EAST -> LinearGradient(
+                xUnit,
+                yUnit,
+                xUnit + CONE_HEIGHT,
+                yUnit,
+                robotDirectionColor,
+                robotDirectionColorTransparent,
+                Shader.TileMode.MIRROR
+            )
+            Directions.SOUTH -> LinearGradient(
+                xUnit,
+                yUnit,
+                xUnit,
+                yUnit - CONE_HEIGHT,
+                robotDirectionColor,
+                robotDirectionColorTransparent,
+                Shader.TileMode.MIRROR
+            )
+            else -> LinearGradient(
+                xUnit,
+                yUnit,
+                xUnit - CONE_HEIGHT,
+                yUnit,
+                robotDirectionColor,
+                robotDirectionColorTransparent,
+                Shader.TileMode.MIRROR
+            )
+        }
+
+        canvas.drawPath(directionTriangle, paint)
+
+        paint.reset()
+
+        paint.color = robotDotInnerColor
+        paint.style = Paint.Style.FILL
+
+        canvas.drawCircle(xUnit, yUnit, INNER_DOT_RADIUS, paint)
+
+        paint.color = white
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = borderWidth
+
+        canvas.drawCircle(xUnit, yUnit, INNER_DOT_RADIUS, paint)
     }
 
     private fun drawGround(canvas: Canvas) {
@@ -75,7 +195,7 @@ class RobotGround(context: Context, attrs: AttributeSet) : View(context, attrs) 
         canvas.drawRect(rect, paint)
 
         paint.color = groundStrokeColor
-        paint.strokeWidth = 2f
+        paint.strokeWidth = borderWidth
         paint.style = Paint.Style.STROKE
 
         canvas.drawRect(getGroundRect(groundWidth, groundHeight), paint)
